8fdccda89756e0a7effb6a9e1f3397008d420be6
EringiShimeji <59910028+EringiShimeji@users.noreply.github.com>
2023-07-08 05:38:59 +0000

    add edit book feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index', as: 'books'
   post 'books' => 'books#create'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id/edit' => 'books#update', as: 'update_book'
   get 'books/:id' => 'books#show', as: 'book'
   delete 'books/:id' => 'books#delete', as: 'delete_book'
   get '/' => 'homes#top'
