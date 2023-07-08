280b8921a1fbf83bb94bd3887cb85105ee5fe283
EringiShimeji <59910028+EringiShimeji@users.noreply.github.com>
2023-07-08 05:43:36 +0000

    create destroy book feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id/edit' => 'books#update', as: 'update_book'
   get 'books/:id' => 'books#show', as: 'book'
-  delete 'books/:id' => 'books#delete', as: 'delete_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
