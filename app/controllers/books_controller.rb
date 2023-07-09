dbca7466049dfb6c7215d0121f60b4c0572e8e56
EringiShimeji <59910028+EringiShimeji@users.noreply.github.com>
2023-07-09 14:05:36 +0000

    impl error flash mesasges on create and update book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,11 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :index
     end
@@ -19,12 +20,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @books = Book.all
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      render :index
+      render :edit
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'books' => 'books#index', as: 'books'
   post 'books' => 'books#create'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id/edit' => 'books#update', as: 'update_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   get 'books/:id' => 'books#show', as: 'book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   get '/' => 'homes#top'
